b2bb09b16b37dc7a5f35db7653244a0b4050c31b
ShivanshuKumarPrajapati <shivanshu.rkt@gmail.com>
2022-12-29 23:38:19 +0530

    create product data & layout for product item

---
+++ components/ProductItem.js
@@ -0,0 +1,28 @@
+/* eslint-disable @next/next/no-img-element */
+import React from 'react'
+import Link  from 'next/link';
+
+const ProductItem = ({product}) => {
+  return (
+    <div className="card">
+      <Link href={`/product/${product.slug}`} passhref>
+        <img
+          src={product.image}
+          alt={product.name}
+          className="rounded shadow"
+              />
+            
+      </Link>
+      <div className="flex flex-col items-center justify-center p-5">
+        <Link href={`/product/${product.slug}`} className="text-lg">
+          {product.name}
+        </Link>
+        <p className="mb-2">{product.brand}</p>
+        <p>$ {product.price}</p>
+        <button className="primary-button">Add to cart</button>
+      </div>
+    </div>
+  );
+}
+
+export default ProductItem;
